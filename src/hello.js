8162c59dbed3ace1d3b123d2a59ecc39d2dc4007
xxxtai <xxxtai@163.com>
2020-06-14 00:35:33 +0800

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,7 @@
-// const got = require('got')
+const got = require('got')
+const URL = 'https://api.github.com/search/repositories'
 module.exports = (pluginContext) => {
-    pluginContext.console.log('verbose', '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    pluginContext.console.log('verbose', URL)
     return (name, env = {}) => {
         return new Promise((resolve, reject) => {
             pluginContext.console.log('info', '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
