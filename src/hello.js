315de0a915b793b001416812d71fcf361cc7680c
xxxtai <xxxtai@163.com>
2020-06-13 12:58:02 +0000

    test

--- src/hello.js
+++ src/hello.js
@@ -1,13 +1,19 @@
 module.exports = (pluginContext) => {
-  return (name, env = {}) => {
-    return new Promise((resolve, reject) => {
-      resolve([
-        {
-          icon: 'fa-hand-spock-o',
-          title: `Hello, ${name}!`,
-          subtitle: 'Please type your name!',
-        },
-      ])
+    pluginContext.console.log('verbose', 'hello world', {
+        ping: 'pong',
     })
-  }
+    return (name, env = {}) => {
+        return new Promise((resolve, reject) => {
+            pluginContext.console.log('info', 'test moda', {
+                ping: 'pong',
+            })
+            resolve([
+                {
+                    icon: 'fa-hand-spock-o',
+                    title: `Hello, ${name}!`,
+                    subtitle: 'Please type your name!',
+                },
+            ])
+        })
+    }
 }
