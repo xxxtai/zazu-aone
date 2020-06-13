d41076db6169ce5081c612086b012397e1cda872
xxxtai <xxxtai@163.com>
2020-06-14 00:42:49 +0800

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -3,7 +3,21 @@ const URL = 'https://api.github.com/search/repositories'
 module.exports = (pluginContext) => {
     pluginContext.console.log('verbose', URL)
     return (name, env = {}) => {
+          const options = {
+    json: true,
+    query: {
+      q: query,
+      per_page: RESULT_ITEMS
+    },
+    headers: {
+      accept: 'application/vnd.github.v3+json'
+    },
+    useElectronNet: false
+  }
         return new Promise((resolve, reject) => {
+            got(URL, options).then((response) => {
+                pluginContext.console.log('info', '&&&&&&&&&&&&&&&', response)
+            }))
             pluginContext.console.log('info', '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
             resolve([
                 {
