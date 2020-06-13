7d705eec3a0c83264e2a6f45be8b2a56347015a8
xxxtai <xxxtai@163.com>
2020-06-13 17:33:36 +0000

    update hello

--- src/hello.js
+++ src/hello.js
@@ -32,15 +32,7 @@ module.exports = (pluginContext) => {
                 pluginContext.console.log('info', '&&&&&&&&&&&&&&&', {
                     data: data
                 })
-
-                resolve([
-                    {
-                        icon: 'fa-hand-spock-o',
-                        title: `Hello, ${name}!`,
-                        subtitle: 'Please type your name!',
-                        value: '88980908******'
-                    },
-                ])
+                resolve(data)
             }).catch((err) => {
                 if (cachedResponse) {
                     resolve(cachedResponse)
