6d6b2f8955a1afc87bf8f8b37b15da4b69405c16
xxxtai <xxxtai@163.com>
2020-06-13 17:11:07 +0000

    update hello

--- src/hello.js
+++ src/hello.js
@@ -17,25 +17,37 @@ module.exports = (pluginContext) => {
             useElectronNet: false
         }
         return new Promise((resolve, reject) => {
-            pluginContext.console.log('verbose', '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+            pluginContext.console.log('verbose', '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', {
+                ping: '0099877))))))'
+            })
             got(URL, options).then((response) => {
+                pluginContext.console.log('info', '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%startffff4444%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+
                 const data = response.body.items.map((repository) => ({
                     id: repository.full_name,
                     title: repository.full_name,
                     value: repository.html_url,
                     subtitle: emoji.emojify(repository.description)
                 }))
-                pluginContext.console.log('info', '&&&&&&&&&&&&&&&', data)
+                pluginContext.console.log('info', '&&&&&&&&&&&&&&&', {
+                    data: data
+                })
+
+                resolve([
+                    {
+                        icon: 'fa-hand-spock-o',
+                        title: `Hello, ${name}!`,
+                        subtitle: 'Please type your name!',
+                        value: '88980908******'
+                    },
+                ])
+            }).catch((err) => {
+                if (cachedResponse) {
+                    resolve(cachedResponse)
+                }
+                reject(err)
             })
-            pluginContext.console.log('info', '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-            resolve([
-                {
-                    icon: 'fa-hand-spock-o',
-                    title: `Hello, ${name}!`,
-                    subtitle: 'Please type your name!',
-                    value: '88980908******'
-                },
-            ])
+            pluginContext.console.log('info', '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%endend%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
         })
     }
 }
